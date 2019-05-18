5df54d9ee3d555a7e27ac376e556c7e5c132875d
abhijeetvhotkar <abhijeetvhotkar@gmail.com>
2019-05-18 18:13:28 -0400

    Updates

---
+++ precache-manifest.45050aa58a6030b825483b8e8386ab97.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "35ed3c9a3acad5908b85a563e98f9d6e",
+    "url": "/portfolio/static/media/abhi.35ed3c9a.jpg"
+  },
+  {
+    "revision": "1b3e9e983c12bec6ed9e",
+    "url": "/portfolio/static/css/main.e3d3245a.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "98b0b341177223264dc1",
+    "url": "/portfolio/static/js/2.98b0b341.chunk.js"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/portfolio/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/portfolio/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/portfolio/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/portfolio/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "1b3e9e983c12bec6ed9e",
+    "url": "/portfolio/static/js/main.1b3e9e98.chunk.js"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/portfolio/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/portfolio/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/portfolio/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/portfolio/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/portfolio/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/portfolio/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/portfolio/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "7d9ecbe894e4251861ae9a08c8a4f78d",
+    "url": "/portfolio/index.html"
+  }
+];
